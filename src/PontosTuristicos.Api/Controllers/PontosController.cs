38d431f2116113d9cdf21db2bac9e9dbbd3aecff
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2025-01-21 00:11:09 -0300

    Criando Projetos e primeiro endpoint
    Criando todos os projetos e o primeiro endpoint de POST.

---
+++ src/PontosTuristicos.Api/Controllers/PontosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PontosTuristicos.Communication.Requests;
+
+namespace pontos_turisticos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PontosController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterPontoJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/PontosTuristicos.Communication/Requests/RequestRegisterPontoJson.cs
@@ -0,0 +1,11 @@
+﻿namespace PontosTuristicos.Communication.Requests
+{
+    public class RequestRegisterPontoJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string City {  get; set; } = string.Empty;
+        public string State {  get; set; } = string.Empty;
+    }
+}
